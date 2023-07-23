2526154760c4d1a0c943dfce1652ea2683a28597
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-07-23 18:44:52 +0300

    Добавил тест авторизации для моб.версии сайта

---
+++ tests/UI/mobile_authorization_web.py
@@ -0,0 +1,18 @@
+from dotenv import dotenv_values
+from selene import browser, by, be, have
+
+
+def test_mobile_authorization_web(mobile_browser_management):
+    dotenv = dotenv_values()
+    browser.open('/')
+    browser.execute_script('document.querySelector(".popmechanic-js-paranja").remove()')
+
+    # WHEN
+    browser.element('#cookie-alert-close button').click()
+    browser.element('[class=h-btn__line]').click()
+    browser.element('a[href="/account/"]').click()
+    browser.element('[name=login]').type(f'{dotenv.get("login")}')
+    browser.element('[name=password]').type(f'{dotenv.get("password")}')
+    browser.element('[data-test=authFormSubmit]').click()
+    # THEN
+    browser.element('.lk-main-top__name').should(have.exact_text('Тест Даниил Тест'))
