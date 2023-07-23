8302b794b77fa44b590236741561c1521447dd2b
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-07-23 18:44:13 +0300

    Добавил фикстуру с настройками для моб.версии сайта

--- tests/UI/conftest.py
+++ tests/UI/conftest.py
@@ -2,9 +2,20 @@ import pytest
 from selene import browser
 
 @pytest.fixture
-def browser_management(scope='function'):
+def desktop_browser_management(scope='function'):
     browser.config.window_width = 1500
     browser.config.window_height = 1200
+    browser.config.base_url = 'https://vodohod.com/'
+    browser.config.timeout = 10
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture
+def mobile_browser_management(scope='function'):
+    browser.config.window_width = 375
+    browser.config.window_height = 667
     browser.config.base_url = 'https://vodohod.com/'
     browser.config.timeout = 10
     yield
