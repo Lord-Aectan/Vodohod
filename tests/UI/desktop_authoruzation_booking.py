4881c00b498d7bc8630cfe3c417cb8cddb2ae3e5
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-07-25 18:18:03 +0300

    переименовал используемую фикстуру в desktop_browser_management_booking

--- tests/UI/desktop_authoruzation_booking.py
+++ tests/UI/desktop_authoruzation_booking.py
@@ -2,17 +2,16 @@ from dotenv import dotenv_values
 from selene import browser, by, be, have
 
 
-def test_authorization_bookinkg(desktop_browser_management):
+def test_authorization_bookinkg(desktop_browser_management_booking):
     dotenv = dotenv_values()
 
-    browser.open('https://booking.vodohod.com/?lang=ru&cruise_id=16823')
+    browser.open('/')
 
     # WHEN
     browser.element('[class^=header-top__btn]').click()
     browser.element('[id=auth_email]').click().type(f'{dotenv.get("login")}')
     browser.element('[id=user_password]').click().type(f'{dotenv.get("password")}')
     browser.element('.b-modal__bottom div').click()
-    # browser.element('[data-v-7370ee61]').click()
 
     # THEN
     browser.element('.header-top__btn--auth span').should(have.exact_text('ТЕСТ ДАНИИЛ ТЕСТ'))
