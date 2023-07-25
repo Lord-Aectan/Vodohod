870e88b20a99b33135a1f54595401bc344e024fe
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-07-25 18:16:34 +0300

    Добавил фикстуру mobile_browser_management_booking переименовал предыдущию фикстуры, для большей наглядности их использования

--- tests/UI/conftest.py
+++ tests/UI/conftest.py
@@ -1,8 +1,9 @@
 import pytest
 from selene import browser
 
+
 @pytest.fixture
-def desktop_browser_management(scope='function'):
+def desktop_browser_management_web():
     browser.config.window_width = 1500
     browser.config.window_height = 1200
     browser.config.base_url = 'https://vodohod.com/'
@@ -13,7 +14,7 @@ def desktop_browser_management(scope='function'):
 
 
 @pytest.fixture
-def mobile_browser_management(scope='function'):
+def mobile_browser_management_web():
     browser.config.window_width = 375
     browser.config.window_height = 667
     browser.config.base_url = 'https://vodohod.com/'
@@ -21,3 +22,23 @@ def mobile_browser_management(scope='function'):
     yield
 
     browser.quit()
+
+@pytest.fixture
+def desktop_browser_management_booking():
+        browser.config.window_width = 1500
+        browser.config.window_height = 1200
+        browser.config.base_url = 'https://booking.vodohod.com/?lang=ru&cruise_id=16823'
+        browser.config.timeout = 10
+        yield
+
+        browser.quit()
+
+@pytest.fixture
+def mobile_browser_management_booking():
+            browser.config.window_width = 495
+            browser.config.window_height = 667
+            browser.config.base_url = 'https://booking.vodohod.com/?lang=ru&cruise_id=16823'
+            browser.config.timeout = 10
+            yield
+
+            browser.quit()
