cbf7485eaf3bb466f316d02a5d77674d73a33fc4
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-07-25 20:38:06 +0300

    Написал тест на проверку работы Таблицы цен

---
+++ tests/UI/visible_price_table.py
@@ -0,0 +1,9 @@
+from selene import browser, by, be, have
+
+
+def test_visible_price_table(desktop_browser_management_web):
+    browser.open('https://vodohod.com/cruises/2024/teplohod-mustaj-karim-09-nov-13-nov-2024/')
+    browser.element('#cookie-alert button').click()
+    # WHEN
+    browser.element('a[data-tab-btn="d-p_prices"]').click()
+    browser.element('#vue-app-price-table').should(be.visible)
