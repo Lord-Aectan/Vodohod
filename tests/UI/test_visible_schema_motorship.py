14f493890b3cb85682da42ea4c2bfc68cde89bb8
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-07-27 14:03:39 +0300

    переменовал файл visible_schema_motorship.py в test_visible_schema_motorship.py

---
+++ tests/UI/test_visible_schema_motorship.py
@@ -0,0 +1,21 @@
+from selene import browser, by, be, have
+import allure
+
+
+def test_visible_schema_motorship(desktop_browser_management_web):
+    with allure.step('Открываем сайт'):
+        browser.open('/')
+    with allure.step('Закрываем окно про куки'):
+        browser.element('#cookie-alert button').click()
+
+    # WHEN
+    with allure.step('Открываем меню со списком теплоходов'):
+        browser.element('a[href="/ships/"]').hover()
+    with allure.step('Выбираем т/х Мустай Карим и открываем его'):
+        browser.element('a[href="/ships/mustai-karim/"]').click()
+    with allure.step('Открываем схему теплохода'):
+        browser.element('.ship-gallery__scheme-image').click()
+
+    # THEN
+    with allure.step('Схема теплохода открылась'):
+        browser.element('.fancybox-image').should(be.visible)
