4f136dc54121cf0895fbcd39081d4ec1c4e60f78
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-07-27 18:39:06 +0300

    Удалил import pprint за ненадобностью. Добавил логгирование статус кода

--- tests/API/test_api_authorization.py
+++ tests/API/test_api_authorization.py
@@ -1,7 +1,7 @@
-import pprint
+import logging
 from dotenv import dotenv_values
 from tests.API.helper import  reqres_session
-
+import pprint
 
 def test_authorization():
     dotenv = dotenv_values()
@@ -10,5 +10,5 @@ def test_authorization():
                                  "login": f'{dotenv.get("login")}',
                                  "password": f'{dotenv.get("password")}'}
                              )
-    pprint.pp(response.json())
+    logging.info(response.json()['code'])
     assert response.status_code == 200
