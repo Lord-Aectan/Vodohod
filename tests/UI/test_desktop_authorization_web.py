c55e2e5389d37e1b1f60e51f273affac3b27e972
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-07-29 19:22:05 +0300

    Добавил разметку теста, поправил allure.step()

--- tests/UI/test_desktop_authorization_web.py
+++ tests/UI/test_desktop_authorization_web.py
@@ -1,6 +1,14 @@
 from tests.authorization.authorization_desktop_form import AuthorizationDesktopForm
+import allure
+from allure_commons.types import Severity
 
 
+
+@allure.tag('web')
+@allure.severity(Severity.CRITICAL)
+@allure.label('owner', 'dmoiseenko')
+@allure.feature('Задачи на авторизацию')
+@allure.title('Успешная авторизация с валидными данными в десктопной версии сайта')
 def test_authorization_form_web(setup_browser, desktop_browser_management_web):
     authorization_desktop_form = AuthorizationDesktopForm()
     # WHEN
