9ab9e61701693fac8d4e11179e6de88e83551065
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-07-29 19:22:23 +0300

    Добавил разметку теста

--- tests/API/test_included_cruise_available_on_site.py
+++ tests/API/test_included_cruise_available_on_site.py
@@ -1,8 +1,16 @@
 import pprint
 from tests.API.helper import  reqres_session
+import allure
+from allure_commons.types import Severity
 
 
+
+@allure.tag('API')
+@allure.severity(Severity.CRITICAL)
+@allure.label('owner', 'dmoiseenko')
+@allure.feature('Задачи на отображение информации по круизам')
+@allure.title('При запросе существующего круиза по нему возвращается информация')
 def test_included_cruise_available_on_site():
-    response =reqres_session.get('/ru/cruises/one/17799')
+    response = reqres_session.get('/ru/cruises/one/17799')
     pprint.pp(response.json())
     assert response.status_code == 200
