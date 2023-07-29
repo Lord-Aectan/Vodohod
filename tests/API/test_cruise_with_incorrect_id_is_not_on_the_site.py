82fc0673125df46866b70e1023133dd2ca046313
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-07-29 19:21:00 +0300

    Добавил разметку теста

--- tests/API/test_cruise_with_incorrect_id_is_not_on_the_site.py
+++ tests/API/test_cruise_with_incorrect_id_is_not_on_the_site.py
@@ -1,7 +1,14 @@
 import pprint
 from tests.API.helper import  reqres_session
+import allure
+from allure_commons.types import Severity
 
 
+@allure.tag('API')
+@allure.severity(Severity.CRITICAL)
+@allure.label('owner', 'dmoiseenko')
+@allure.feature('Задачи на отображение информации по круизам')
+@allure.title('Если круиза не существует, то вернётся \'Круиз не найден\'')
 def test_cruise_with_incorrect_id_is_not_on_the_site():
     response = reqres_session.get('/ru/cruises/one/84848')
     pprint.pp(response.json())
