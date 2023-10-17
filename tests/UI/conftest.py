d324fe4cf23b1b62a3b0f5a582b39446c86356c1
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-10-17 10:44:00 +0300

    Заменил круиз для проверки авторизации на букинг

--- tests/UI/conftest.py
+++ tests/UI/conftest.py
@@ -84,7 +84,7 @@ def mobile_browser_management_web():
 def desktop_browser_management_booking():
     browser.config.window_width = 1500
     browser.config.window_height = 1200
-    browser.config.base_url = 'https://booking.vodohod.com/?lang=ru&cruise_id=16823'
+    browser.config.base_url = 'https://booking.vodohod.com/?lang=ru&cruise_id=17565'
     browser.config.timeout = 10
 
     yield
@@ -99,7 +99,7 @@ def desktop_browser_management_booking():
 def mobile_browser_management_booking():
     browser.config.window_width = 495
     browser.config.window_height = 667
-    browser.config.base_url = 'https://booking.vodohod.com/?lang=ru&cruise_id=16823'
+    browser.config.base_url = 'https://booking.vodohod.com/?lang=ru&cruise_id=17565'
     browser.config.timeout = 10
 
     yield
